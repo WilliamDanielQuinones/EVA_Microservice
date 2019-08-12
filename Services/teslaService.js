1aa211dde90313be082a3feb3e533e93447845ce
danqbucket <williamdanielquinones@gmail.com>
2019-08-11 20:27:33 -0400

    Add tesla service and endpoints list

--- Services/teslaService.js
+++ Services/teslaService.js
@@ -1,14 +1,29 @@
 'use strict'
-import axios from 'axios'
 
-const teslaURL = 'https://owner-api.teslamotors.com'
+const ENDPOINTS = require('../teslaEndpoints').ENDPOINTS
+const axios = require('axios')
 
-modules.export = {
-  getAccessToken() {
-    const tokenPath = '/oauth/token'
+const BASE_URL = 'https://owner-api.teslamotors.com/'
+
+module.exports = {
+  async getAccessToken(account, client) {
+    const tokenUrl = BASE_URL + ENDPOINTS.AUTHENTICATE.URI
+    let payload = {
+      email: account.email,
+      password: account.password,
+      client_id: client.clientId,
+      client_secret: client.clientSecret,
+      grant_type: 'password'
+    }
+    try{
+      let resp = await axios.post(tokenUrl, payload)
+      if (resp) return resp.data
+    } catch (error) {
+        return null
+    }
   },
 
   revokeAccessToken() {
-    const revokeTokenPath = '/oauth/revoke'
+    const revokeTokenUrl = BASE_URL + ENDPOINTS.REVOKE_AUTH_TOKEN.URI
   }
 }
